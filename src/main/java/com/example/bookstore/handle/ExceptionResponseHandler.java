f32685638fb91e23bab525d7229e20c1702ec435
Kachain Jantalat <kachain.j@kbtg.tech>
2020-12-04 11:18:26 +0700

    [ADD] Exception handler

---
+++ src/main/java/com/example/bookstore/handle/ExceptionResponseHandler.java
@@ -0,0 +1,18 @@
+package com.example.bookstore.handle;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class ExceptionResponseHandler {
+
+    @ExceptionHandler({RuntimeException.class})
+    public ResponseEntity<Exception> accountNotFound(
+            RuntimeException exception
+    ) {
+        Exception error = new Exception();
+        error.setCode(404);
+        error.setMessage(exception.getMessage());
+        return new ResponseEntity<Exception>(error, HttpStatus.OK);
+    }
+}
