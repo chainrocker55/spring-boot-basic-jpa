c1e317dec3d1d3e37ba73b1dbe0fe574ac02aa51
Kachain Jantalat <kachain.j@kbtg.tech>
2020-12-04 11:00:51 +0700

    [Update Code] BookController and response

--- src/main/java/com/example/bookstore/controller/BookController.java
+++ src/main/java/com/example/bookstore/controller/BookController.java
@@ -1,4 +1,47 @@
 package com.example.bookstore.controller;
 
+import com.example.bookstore.entiry.Book;
+import com.example.bookstore.repository.BookRepository;
+import com.example.bookstore.responsemodel.BookResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class BookController {
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping("/v1/book/{id}")
+    public BookResponse getBookById(@PathVariable int id) {
+
+        Optional<Book> book = bookRepository.findById(id);
+        if (book.isPresent()) {
+            BookResponse bookResponse = new BookResponse();
+            bookResponse.setBookId(book.get().getId());
+            bookResponse.setBookName(book.get().getBookName());
+            bookResponse.setPrice(book.get().getPrice());
+            bookResponse.setNumBook(book.get().getNumBook());
+            return bookResponse;
+        }
+        throw new RuntimeException("Book not found with id=" + id);
+
+    }
+
+    @GetMapping("/v1/books")
+    public List<Book> getBooks() {
+
+        Iterable<Book> book = bookRepository.findAll();
+        List<Book> bookList = new ArrayList<>();
+        book.forEach(bookList::add);
+        return bookList;
+
+    }
+
+
 }
--- src/main/java/com/example/bookstore/responsemodel/BookResponse.java
+++ src/main/java/com/example/bookstore/responsemodel/BookResponse.java
@@ -1,4 +1,40 @@
 package com.example.bookstore.responsemodel;
 
 public class BookResponse {
+    private int bookId;
+    private String bookName;
+    private int price;
+    private int numBook;
+
+    public int getNumBook() {
+        return numBook;
+    }
+
+    public void setNumBook(int numBook) {
+        this.numBook = numBook;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
