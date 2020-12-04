27f3e44aa8a815ae18bb754404b0b3499dca8717
Kachain Jantalat <kachain.j@kbtg.tech>
2020-12-04 10:11:01 +0700

    [Test Build] start project and check bean

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -2,12 +2,16 @@ package com.example.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookStoreApplication.class, args);
+		ConfigurableApplicationContext ctx
+				= SpringApplication.run(BookStoreApplication.class, args);
+
+		System.out.println(ctx.getBeanDefinitionCount());
 	}
 
 }
